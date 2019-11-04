5ca5ac783d29b83d0b1b79f0c4806fc75a5127b1
Mufasa <fashioncstar2019@outlook.com>
2019-11-04 22:58:48 +0800

    step 0
    basic code( empty project)

---
+++ app/src/main/java/social/media/railwaystation/MainActivity.java
@@ -0,0 +1,13 @@
+package social.media.railwaystation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
